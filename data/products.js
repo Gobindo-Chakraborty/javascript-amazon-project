357a31fadc32d1bbbbba18b33a1b217b9ca0cd60
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-01-20 10:26:56 +0600

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import formatCurrency from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
   products.forEach((product) => {
@@ -7,6 +9,28 @@ export function getProduct(productId) {
   });
 
   return matchingProduct;
+}
+
+class Product {
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -480,4 +504,6 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
